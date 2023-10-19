008795a363acaa31cab59cdbf8a35596c005171c
CarlosCardenas24 <nooptions101@gmail.com>
2023-10-19 00:21:17 -0700

    19 minutes into video

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,53 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Outlet, LiveReload, Link, Links, Meta} from '@remix-run/react'
+import globalStylesUrl from './styles/global.css'
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+export const links = () => [{rel: 'stylesheet', href:
+globalStylesUrl }]
 
 export default function App() {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
+    <Document>
+      <Layout>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+      </Layout>
+    </Document>
+  )
+}
+
+function Document({ children, title } : any) {
+  return (
+    <html lang='en'>
+        <head>
+          <Links />
+          <title>My Remix Blog</title>
+        </head>
+        <body>
+          {children}
+          {process.env.NODE_ENV === 'development' ?
+          <LiveReload /> : null}
+        </body>
+      </html>
+  )
+}
+
+function Layout({children} : any) {
+  return (
+    <>
+      <nav className='navbar'>
+        <Link to='/' className='logo'>
+          Remix
+        </Link>
+
+        <ul className='nav'>
+          <li>
+            <Link to='/posts'>Posts</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="container">
+        {children}
+      </div>
+    </>
+  )
 }
